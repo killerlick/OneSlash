d6c0b48a32347674f23bccfca9c9ce07ca5c4524
malick <chimpanfeu@gmail.com>
2025-12-18 04:06:31 -0500

    creation de script pour enemy

---
+++ Script/Opponent.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Opponent
+
+@export var sprite2D :Texture2D;
+@export var opponent_name :String;
+
+@export var number_phase = 1 ;
+@export var reaction_time : float = 3.0;
+@export var delay= 0;
+@export var time_remaining : float = 7.0;
