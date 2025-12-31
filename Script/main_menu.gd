e73866294f95500c913b3961662b5723d01db1ae
malick <chimpanfeu@gmail.com>
2025-12-31 02:06:58 -0500

    ajout d'image pour le jeu

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
+@onready var animation_character : AnimationPlayer = $Image_title/AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_character.play("lobby")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
