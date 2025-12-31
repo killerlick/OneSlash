69f4b1214fd9cc89aef19de299bb4e349f410ae7
malick <chimpanfeu@gmail.com>
2025-12-31 03:01:43 -0500

    son slash + main menu

--- Script/main_character.gd
+++ Script/main_character.gd
@@ -4,6 +4,7 @@ class_name Main_character
 @onready var animation = $AnimationPlayer
 @onready var not_sure = $Not_sure
 @onready var sure = $Sure
+@onready  var slash_sound : AudioStreamPlayer = $Slash_sound
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,6 +16,7 @@ func missed() -> void:
 	animation.play("missed")
 
 func slash() -> void:
+	play_slash_sound()
 	animation.play("slash")
 	pass
 
@@ -29,3 +31,9 @@ func show_sign(sign : String ) -> void :
 func hide_sign( ) -> void :
 	not_sure.visible = false
 	sure.visible = false
+
+func play_slash_sound():
+	slash_sound.set_pitch_scale(randf_range(0.99 , 1.05))
+	slash_sound.play()
+	
+	pass
