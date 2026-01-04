4c0ded10d6445b1d6ae2617dd0ed546f1284258e
malick <chimpanfeu@gmail.com>
2026-01-03 21:24:50 -0500

    ajout de plus de visuel pour le jeu et commencement du gameplay

---
+++ Script/OpponentPhase.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name OpponentPhase
+
+@export var hit : int = 1
+@export var feint : int = 0
--- Script/opponent_ressources.gd
+++ Script/opponent_ressources.gd
@@ -10,8 +10,24 @@ signal cd
 @export var sprite_defeated: Texture2D
 
 
+@export var phase : Array[OpponentPhase] = []
+
+
 @export var number_hit_by_phase : Array[int] = [] ;
 
+#le temps requis a l'ennemi pour reagir
 @export var nb_reaction_time  : float = 2.0;
+
+#le delain utilisé par l'ennemi avant de commencer ces assaut
 @export var nb_delay          : float = 1.0;
+
+#la durée dans laquel l'ennemi va attaqueé
 @export var nb_time_remaining : float = 7.0;
+
+
+func _ready() -> void:
+	if phase == null :
+		var test = OpponentPhase.new()
+		test.feint = 0
+		test.hit = number_hit_by_phase[0]
+		phase.append(test)
